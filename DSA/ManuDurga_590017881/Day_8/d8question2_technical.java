17ba35280777d5b31319a3ecb83d97f7622c8428
ManuDurga <you@example.com>
2026-06-22 03:55:01 +0530

    Name fix

--- DSA/ManuDurga_590017881/Day_8/d8question2_technical.java
+++ DSA/ManuDurga_590017881/Day_8/d8question2_technical.java
@@ -2,7 +2,7 @@ package Day_8;
 
 import java.util.Scanner;
 
-public class d8question_technical {
+public class d8question2_technical {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();          // size of the input array
